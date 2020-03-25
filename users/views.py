fc213205a998203e19e61e979f427363d5ca37cc
Shoumik <shoumikgandre@gmail.com>
2020-03-26 00:23:17 +0530

    editable profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -21,4 +21,20 @@ def register(request):
 
 @login_required(login_url='users:login')
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, "Your account has been updated")
+            return redirect('users:profile')
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': user_update_form,
+        'p_form': profile_update_form
+    }
+    return render(request, 'users/profile.html', context)
