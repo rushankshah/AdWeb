e0d2e567d7d600c5fac8d78224e7889369488c79
Rushank Shah <rushankshah65@gmail.com>
2020-04-08 15:15:05 +0530

    Some CSS changes and chart Trial made.

--- adtool/static/adtool/js/example.js
+++ adtool/static/adtool/js/example.js
@@ -1,3 +1,39 @@
-ads.forEach(element => {
-    document.getElementById('example').innerHTML += "<h2>" + element.fields.name + "</h2>" + "<h2>" + element.fields.clicks + "</h2>";
-});
+import "core-js";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+
+let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+ads.forEach(element=>{
+    chart.data = chart.data + {"name":element.fields.name,"clicks":element.fields.clicks};
+})
+
+let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "name";
+categoryAxis.title.text = "Name of Ad";
+let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.dataFields.category = "clicks";
+valueAxis.title.text = "Number of clicks";
+
+let series = chart.series.push(new am4charts.ColumnSeries());
+series.name = "Number of clicks per ad";
+series.columns.template.tooltipText = "Series: {name}\nCategory: {categoryX}\nValue: {valueY}";
+series.columns.template.fill = am4core.color("#104547");
+series.dataFields.valueY = "clicks";
+series.dataFields.categoryX = "name";
+
+chart.cursor = new am4charts.XYCursor();
+
+
+// ads.forEach(element => {
+//     document.getElementById('example').innerHTML += "<h2>" + element.fields.name + "</h2>" + "<h2>" + element.fields.clicks + "</h2>";
+// });
+// var names = [];
+// var clicks = [];
+// ads.forEach(element =>{
+//     names.push(element.fields.name);
+//     clicks.push(element.fields.clicks);
+// }
+// )
+// console.log(names);
+// console.log(clicks);
