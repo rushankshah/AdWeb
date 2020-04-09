75a717a9e2f9a1a137de1aa5b41c607a5332f3a3
Shoumik <shoumikgandre@gmail.com>
2020-04-09 17:44:47 +0530

    chart trial

--- adtool/static/adtool/js/example.js
+++ adtool/static/adtool/js/example.js
@@ -1,3 +1,30 @@
+// ads.forEach(element => {
+//     document.getElementById('example').innerHTML += "<h2>" + element.fields.name + "</h2>" + "<h2>" + element.fields.clicks + "</h2>";
+// });
+
+var data = []
+
 ads.forEach(element => {
-    document.getElementById('example').innerHTML += "<h2>" + element.fields.name + "</h2>" + "<h2>" + element.fields.clicks + "</h2>";
+    data.push({ label: element.fields.name, y: element.fields.clicks });
 });
+
+window.onload = function () {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        theme: "light1", // "light2", "dark1", "dark2"
+        animationEnabled: false, // change to true		
+        title: {
+            text: "Basic Column Chart"
+        },
+        data: [
+            {
+                // Change type to "bar", "area", "spline", "pie",etc.
+                type: "column",
+                dataPoints: data
+
+            }
+        ]
+    });
+    chart.render();
+
+}
