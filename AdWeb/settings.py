d81ba6b9a16250367842e2c011a3cdf60deec318
Shoumik <shoumikgandre@gmail.com>
2020-04-27 20:40:28 +0530

    changing image on upload or update advertisement according to the selected ad

--- AdWeb/settings.py
+++ AdWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5)zaq4_yue3ievvp6__u=+=djq2)t3bp^=fkl4s)rj=0j^d4hc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
